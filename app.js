89fd658b028320f801f6e18cd5c7a72ff9f1275b
vaibhavi.joshi <vaibhavi.joshi@infostretch.com>
2016-07-13 12:49:01 +0530

    change app id

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var restify = require('restify');
 var builder = require('botbuilder');
 
 // Create bot and add dialogs
-var bot = new builder.BotConnectorBot({ appId: 'ceef4aa2-21d2-43d8-a1f3-d7250bec3dfc', appSecret: 'MJNhfc9iUbdjAHKemj7Fmkq' });
+var bot = new builder.BotConnectorBot({ appId: 'ILSI_123', appSecret: 'MJNhfc9iUbdjAHKemj7Fmkq' });
 bot.add('/', function (session) {
     session.send('Hello World');
 });
