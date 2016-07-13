f00aef0e4bce55e86a6781b40bdd311daacf44fa
vaibhavi.joshi <vaibhavi.joshi@infostretch.com>
2016-07-13 12:37:48 +0530

    chnaged appid and secret

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var restify = require('restify');
 var builder = require('botbuilder');
 
 // Create bot and add dialogs
-var bot = new builder.BotConnectorBot({ appId: '010da587-53aa-4e52-9718-910b9a8e0a3b', appSecret: 'YHi9v8fq5Qfv7WenLTMuTh1' });
+var bot = new builder.BotConnectorBot({ appId: 'ceef4aa2-21d2-43d8-a1f3-d7250bec3dfc', appSecret: 'MJNhfc9iUbdjAHKemj7Fmkq' });
 bot.add('/', function (session) {
     session.send('Hello World');
 });
