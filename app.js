de75fcc09584f716088a4a27a9541d088d476e38
vaibhavi.joshi <vaibhavi.joshi@infostretch.com>
2016-07-13 15:55:00 +0530

    updated package.json

--- app.js
+++ app.js
@@ -6,17 +6,20 @@ var restify = require('restify');
 //=========================================================
 // Setup Restify Server
 var server = restify.createServer();
-var bot = new builder.UniversalBot(connector);
-server.post('/api/messages', connector.listen());
+
+
 var port = process.env.PORT || 8082;
 
 //var connector = new builder.ConsoleConnector().listen();
+
 // Create chat bot
 var connector = new builder.ChatConnector({
     appId: 'ceef4aa2-21d2-43d8-a1f3-d7250bec3dfc',
     appPassword: 'MJNhfc9iUbdjAHKemj7Fmkq'
 });
+var bot = new builder.UniversalBot(connector);
 
+server.post('/api/messages', connector.listen());
 server.post('/api/messages', connector.listen());
 server.listen(port, function () {
    console.log('%s listening to %s', server.name, server.url); 
