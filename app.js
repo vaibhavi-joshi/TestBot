2e3393d3369619f74c91435f283038b316e482d8
vaibhavi.joshi <vaibhavi.joshi@infostretch.com>
2016-07-14 13:23:38 +0530

    replace choice with text

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ bot.dialog('/login',[ function (session) {
 		
          console.log('login call back is called');
 			if(token == null) {
-				builder.Prompts.choice(session, 'Something went wrong while logging in. Retry?', ["yes","no"]);
+				builder.Prompts.text(session, 'Something went wrong while logging in. Retry? ans in yes or no');
                next();
 			}
 			else {
