7efedf390ce27ab3046ab8d7c0ae4a5f7c3f2ce0
vaibhavi.joshi <vaibhavi.joshi@infostretch.com>
2016-07-14 13:20:38 +0530

    remove cosnole connector

--- app.js
+++ app.js
@@ -22,18 +22,18 @@ var current_leave_count = null;
 var server = restify.createServer();
 var port = process.env.PORT || 8082;
 
-var connector = new builder.ConsoleConnector().listen();
+//var connector = new builder.ConsoleConnector().listen();
 // Create chat bot
-// var connector = new builder.ChatConnector({
-//     appId: 'ceef4aa2-21d2-43d8-a1f3-d7250bec3dfc',
-//     appPassword: 'MJNhfc9iUbdjAHKemj7Fmkq'
-// });
+var connector = new builder.ChatConnector({
+    appId: 'ceef4aa2-21d2-43d8-a1f3-d7250bec3dfc',
+    appPassword: 'MJNhfc9iUbdjAHKemj7Fmkq'
+});
 var bot = new builder.UniversalBot(connector);
 
-// server.post('/api/messages', connector.listen());
-// server.listen(port, function () {
-//    console.log('%s listening to %s', server.name, server.url); 
-// });
+server.post('/api/messages', connector.listen());
+server.listen(port, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
 
 //=========================================================
 // Luis Setup
